346b7e4090a16cb81ce643328b06bbc130b9b2c4
Ricardo Cambui <r.cambui@hotmail.com>
2023-01-17 14:54:04 -0300

    Exercício adicionado

--- src/Iniciante/Beecrowd1008.java
+++ src/Iniciante/Beecrowd1008.java
@@ -1,5 +1,6 @@
 package Iniciante;
 
+
 /*Escreva um programa que leia o número de um funcionário, seu número de horas 
 trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
 A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
@@ -15,10 +16,12 @@ um espaço em branco antes e depois da igualdade. No caso do salário, também
 deve haver um espaço em branco após o $. */
 
 import java.util.Scanner;
+import java.io.IOException;
+
 
 public class Beecrowd1008 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         Scanner scan = new Scanner(System.in);
         
